7f1402679ee10110987bf4ce6033299ec75b4245
SanketRP <sanketrp123@gmail.com>
2023-04-13 13:13:48 +0530

    Completeed the auth reducer and set up the setAuthToken utility function

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -9,22 +9,34 @@ import "./App.css";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<Navbar />
-				<Alert />
-				<Routes>
-					<Route path='/' element={<Landing />} />
-					{/* From here add each element a section.container */}
-					<Route path='register' element={<Register />} />
-					<Route path='login' element={<Login />} />
-				</Routes>
-			</Fragment>
-		</Router>
-	</Provider>
-);
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Alert />
+					<Routes>
+						<Route path='/' element={<Landing />} />
+						{/* From here add each element a section.container */}
+						<Route path='register' element={<Register />} />
+						<Route path='login' element={<Login />} />
+					</Routes>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,34 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+
+	try {
+		const res = await axios.get("/api/auth");
+
+		dispatch({
+			type: USER_LOADED,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: AUTH_ERROR,
+		});
+	}
+};
 
 // Register User
 export const register =
@@ -35,3 +63,33 @@ export const register =
 			});
 		}
 	};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	const body = JSON.stringify({ email, password });
+
+	try {
+		const res = await axios.post("api/auth", body, config);
+
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: LOGIN_FAIL,
+		});
+	}
+};
