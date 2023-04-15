ae3c4ee29a3c358451009d1553ac8b0382d552cf
SanketRP <sanketrp123@gmail.com>
2023-04-15 19:08:46 +0530

    Fixed bugs

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,6 @@ const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
-// const normalize = require("normailze-url");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
